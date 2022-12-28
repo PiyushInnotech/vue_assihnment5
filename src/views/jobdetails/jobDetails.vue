591cc021269457dd3ee4b8e573e50428e6f49ca7
unknown <piyushmangla@innotechteam.in>
2022-12-28 16:22:48 +0530

    adding dynamic routing to job details and notfound page

---
+++ src/views/jobdetails/jobDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="jobDetails">
+    <h1>This is job details.</h1>
+    <h1>job id: {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "jobDetails",
+  components: {},
+  props: ["id"],
+};
+</script>
--- src/views/jobs.vue
+++ src/views/jobs.vue
@@ -2,9 +2,11 @@
   <div class="jobs">
     <h1 class="jobsHead">JOBS</h1>
     <div class="jobWrapper">
-    <div v-for="job in jobs" :key="job.id" class="jobHead">
-      <h2 >  {{job.title}}   {{job.details}} </h2>
-    </div>
+      <div v-for="job in jobs" :key="job.id" class="jobHead">
+        <router-link :to="{ name: 'jobDetails', params: { id: job.id } }"
+          ><h2>{{ job.title }}</h2></router-link
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -25,24 +27,29 @@ export default {
 </script>
 
 <style scoped>
-.jobsHead{
-    margin-top: 40px;
-    font-size: 50px;
-    letter-spacing: 1px;
+.jobsHead {
+  margin-top: 40px;
+  font-size: 50px;
+  letter-spacing: 1px;
 }
-.jobWrapper{
-    margin-top: 50px;
-    display: flex;
-    text-align: center;
-    flex-direction: column;
-    align-items: center;
+.jobWrapper {
+  margin-top: 50px;
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  align-items: center;
 }
-.jobHead{
-    font-size: 20px;
-    margin-top: 10px;
-    width: 35%;
-    background-color: #d4d6d9;
-    color: #3f4040;
-    border-radius: 5px;
+.jobHead {
+  font-size: 20px;
+  margin-top: 10px;
+  width: 35%;
+  background-color: #d4d6d9;
+  color: #3f4040;
+  border-radius: 5px;
+}
+.jobHead a {
+  color: #2c3e50;
+  text-decoration: none;
+  border-radius: 5px;
 }
 </style>
